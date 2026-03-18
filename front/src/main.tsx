bd13f99ddbdcc1c86c745189914ae33844484043
Anora Ozodova <monyamin006@gmail.com>
2026-03-19 00:51:16 +0500

    добавила шрифты и глобальные стили

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './assets/styles/roots.css'
+import './assets/fonts/stylesheet.css'
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
